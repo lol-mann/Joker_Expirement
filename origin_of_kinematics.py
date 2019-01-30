4ee61b1c6a66e1bc3a31462afc6bd9889bed90de
lol-mann <46281390+lol-mann@users.noreply.github.com>
2019-01-30 19:38:25 +0530

    Update origin_of_kinematics.py

--- origin_of_kinematics.py
+++ origin_of_kinematics.py
@@ -26,38 +26,3 @@ class Intro_scene(Scene):
         self.wait(1)
         self.play(FadeIn(example_tex))
         self.wait(2)
-class SimplePendulum(VMobject):
-    CONFIG = {
-        "buff": 0.5,
-        "start_angles": [PI / 6],
-        "color1": RED,
-    }
-
-    def __init__(self,pos=ORIGIN,length=2,mass_of_bob=1,gravity=9.81,damping=0, **kwargs):
-        VMobject.__init__(self, **kwargs)
-        line1 = Line(ORIGIN, UP,stroke_width=1,stroke_color=BLACK)
-        dot1 = Dot(color=self.color1).scale(1.5)
-        dot1.add_updater(lambda d: d.move_to(line1.get_end()))
-
-        self.add(line1, dot1)
-        self.L1 = length
-        self.m1 = mass_of_bob
-        self.c = gravity/length
-        self.b = damping
-        self.g = gravity
-        self.time=0
-        self.time_list = []
-        def update(group, dt):
-            self.time+=dt
-            y0 = [np.pi/12, 0.0]
-            line1 = group.submobjects[0]
-            self.time_list.append(self.time)
-            sol = odeint(deriv, y0, self.time_list, args=(self.b, self.c))
-            p1 = np.sin(sol[-1, 0])* RIGHT * 2 + np.cos(sol[-1, 0]) * DOWN * 2
-            line1.put_start_and_end_on(pos, pos+p1)
-
-        def deriv(y, t, b, c):
-            theta, omega = y
-            dydt = [omega, -b*omega - c*np.sin(theta)]
-            return dydt
-        self.add_updater(update)
